f1cb51c6bae3bdd68e332aef087b6370252edb78
Emanuel <emaw_705@hotmail.com>
2020-06-25 18:00:21 -0300

    Version inicial - unidad-4-jenkins

--- docker/mysql/data/init.sql
+++
@@ -1,4 +0,0 @@
--- create the databases
-CREATE DATABASE IF NOT EXISTS dbname;
-USE dbname;
-CREATE TABLE employees (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100) NOT NULL, address VARCHAR(255) NOT NULL, salary INT(10) NOT NULL );
